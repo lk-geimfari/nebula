79ffeb27317d7ccbbd7bedf6968784fc59ba5b27
Lk Geimfari <likid.geimfari@gmail.com>
2016-08-28 17:32:18 +0300

    Minor changes

--- farbe.erl
+++ farbe.erl
@@ -3,29 +3,53 @@
 
 
 -export ([cprint/1]).
+-export ([background/1]).
+-export ([underline/1]).
 
 
-cprint({black,  Str})  ->
-  io:format("\033[0m\033[30m~s\033[30m\033[0m~n", [Str]);
+underline(Str) -> io:format("\e[4m~s\033[0m~n", [Str]).
+
+cprint({black,  Str}) ->
+  io:format("\033[30m~s\033[0m~n", [Str]);
 cprint({red,    Str})  ->
-  io:format("\033[0m\033[31m~s\033[31m\033[0m~n", [Str]);
+  io:format("\e[31m~s\033[0m~n",   [Str]);
 cprint({green,  Str})  ->
-  io:format("\033[0m\033[92m~s\033[92m\033[0m~n", [Str]);
+  io:format("\033[92m~s\033[0m~n", [Str]);
 cprint({dgreen, Str})  ->
-  io:format("\033[0m\033[32m~s\033[32m\033[0m~n", [Str]);
+  io:format("\033[32m~s\033[0m~n", [Str]);
 cprint({orange, Str})  ->
-  io:format("\033[0m\033[33m~s\033[33m\033[0m~n", [Str]);
+  io:format("\033[33m~s\033[0m~n", [Str]);
 cprint({blue,   Str})  ->
-  io:format("\033[0m\033[34m~s\033[34m\033[0m~n", [Str]);
+  io:format("\033[34m~s\033[0m~n", [Str]);
 cprint({purple, Str})  ->
-  io:format("\033[0m\033[35m~s\033[35m\033[0m~n", [Str]);
+  io:format("\033[35m~s\033[0m~n", [Str]);
 cprint({yellow, Str})  ->
-  io:format("\033[0m\033[93m~s\033[93m\033[0m~n", [Str]);
+  io:format("\033[93m~s\033[0m~n", [Str]);
 cprint({pink,   Str})  ->
-  io:format("\033[0m\033[95m~s\033[95m\033[0m~n", [Str]);
+  io:format("\033[95m~s\033[0m~n", [Str]);
 cprint({cyan,   Str})  ->
-  io:format("\033[0m\033[96m~s\033[96m\033[0m\033[0m~n", [Str]);
+  io:format("\033[96m~s\033[0m~n", [Str]);
 cprint({grey,   Str})  ->
-  io:format("\033[0\033[37m~s\033[37m\033[0m", [Str]);
+  io:format("\033[37m~s\033[0m",   [Str]);
 cprint({Other,  Str})  ->
+  io:format("~s~n", [Str]).
+
+
+background({black, Str})  ->
+  io:format("\e[40m~s\033[0m", [Str]);
+background({red, Str})    ->
+  io:format("\e[41m~s\033[0m", [Str]);
+background({green, Str})  ->
+  io:format("\e[42m~s\033[0m", [Str]);
+background({yellow, Str}) ->
+  io:format("\e[43m~s\033[0m", [Str]);
+background({blue, Str})   ->
+  io:format("\e[44m~s\033[0m", [Str]);
+background({purple, Str}) ->
+  io:format("\e[45m~s\033[0m", [Str]);
+background({cyan, Str})   ->
+  io:format("\e[46m~s\033[0m", [Str]);
+background({red, Str})    ->
+  io:format("\e[41m~s\033[0m", [Str]);
+background({Other,  Str})  ->
   io:format("~s~n", [Str]).
