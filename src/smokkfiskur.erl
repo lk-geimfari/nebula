d2eca3d7d82446bfd8b01fad318b510e6378aa3c
Lk Geimfari <likid.geimfari@gmail.com>
2016-10-10 20:03:43 +0300

    Added macros for println function

--- src/smokkfiskur.erl
+++ src/smokkfiskur.erl
@@ -1,38 +1,39 @@
+%%%-------------------------------------------------------------------
+%%% @author Lk Geimfari <likid.geimfari@gmail.com>
+%%% @copyright (C) 2016, Lk Geimfari (Isaak Uchakaev)
+%%%-------------------------------------------------------------------
 -module(smokkfiskur).
+-author("Lk Geimfari").
 
--export ([println/1]).
--export ([background/1]).
-
-
-%%====================================================
-%% Functions for colored output
-%%====================================================
-
-println({black,  Str})  ->
-  io:format("\033[30m~s\033[0m~n", [Str]);
-println({red,    Str})  ->
-  io:format("\e[31m~s\033[0m~n",   [Str]);
-println({green,  Str})  ->
-  io:format("\033[92m~s\033[0m~n", [Str]);
-println({orange, Str})  ->
-  io:format("\033[33m~s\033[0m~n", [Str]);
-println({blue,   Str})  ->
-  io:format("\033[34m~s\033[0m~n", [Str]);
-println({purple, Str})  ->
-  io:format("\033[35m~s\033[0m~n", [Str]);
-println({yellow, Str})  ->
-  io:format("\033[93m~s\033[0m~n", [Str]);
-println({cyan,   Str})  ->
-  io:format("\033[96m~s\033[0m~n", [Str]);
-println({grey,   Str})  ->
-  io:format("\033[37m~s\033[0m",   [Str]);
-println({Other,  Str})  ->
-  io:format("~s~n", [Str]).
+-export ([println/1, background/1]).
 
 
 %%====================================================
-%% Functions for colored  background.
+%% Internal macros for colored text.
 %%====================================================
+-define (BLACK(S),  "\033[30m~s~n", [S]).
+-define (RED(S),    "\e[31m~s~n",   [S]).
+-define (GREEN(S),  "\033[92m~s~n", [S]).
+-define (ORANGE(S), "\033[33m~s~n", [S]).
+-define (BLUE(S),   "\033[34m~s~n", [S]).
+-define (PURPLE(S), "\033[35m~s~n", [S]).
+-define (YELLOW(S), "\033[93m~s~n", [S]).
+-define (CYAN(S),   "\033[96m~s~n", [S]).
+-define (GREY(S),   "\033[37m~s~n", [S]).
+-define (_OTHER(S), S).
+
+
+println({black,  S})  -> io:format(?BLACK(S));
+println({red,    S})  -> io:format(?RED(S));
+println({green,  S})  -> io:format(?GREEN(S));
+println({orange, S})  -> io:format(?ORANGE(S));
+println({blue,   S})  -> io:format(?BLUE(S));
+println({purple, S})  -> io:format(?PURPLE(S));
+println({yellow, S})  -> io:format(?YELLOW(S));
+println({cyan,   S})  -> io:format(?CYAN(S));
+println({grey,   S})  -> io:format(?GREY(S));
+println({Other,  S})  -> io:format(?_OTHER(S)).
+
 
 background({black, Str})  ->
   io:format("\e[40m~s\033[0m", [Str]);
