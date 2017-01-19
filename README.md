4233d1842b603b9364d797e6b5b232ec4411fb20
Likid Geimfari <likid.geimfari@gmail.com>
2017-01-19 23:30:02 +0300

    Refactoring

--- README.md
+++ README.md
@@ -8,49 +8,57 @@
   <img src="https://raw.githubusercontent.com/lk-geimfari/smokkfiskur/master/other/logo2.png">
 </p>
 ---------
-__Smokkfiskur__ is a small library for colored (ANSI) output in Erlang. It's can be useful when you need to create a command-line applications.
+__Smokkfiskur__ is a small library for colored (ANSI) output in Erlang. It's can be useful when you 
+need to create a command-line applications.
 
 
 ### Installation
 Make sure, that `rebar3` installed and configured in your OS. If not, then [here](https://medium.com/@brucify/using-rebar3-to-manage-erlang-packages-282f78adff1e#.pyz8i9cvy) you can find good manual. 
 
 Add `{deps, [{smokkfiskur, “0.1.3”}]}.` to your `rebar.config` and run the following command:
+
 ```
 ➜  ~  rebar3 compile
 ```
 
 
 ### Usage
-Just use the following format {color, "Text"}
+Just use the following format `print(color, "Text").`
+
 ```erlang
 smokkfiskur:print({red, "Colored text"}).
 ```
 ---------
 ### Example
 ```erlang
--import(smokkfiskur, [print/1]).
+-import(smokkfiskur, [print/2]).
 
 update_something() ->
   %% ...
   %% ...
   %% ...
-  print({green, "Something has been updated successfully!"}).
+  print(green, "Something has been updated successfully!").
 ```
 or
+
 ```erlang
--import(smokkfiskur, [print/1]).
+-import(smokkfiskur, [print/2]).
 
 
 %%=========================
 %% Text
 %%=========================
-print({red,    "Text"}).
-print({black,  "Text"}).
+print(red,    "Text").
+print(black,  "Text").
 
 %%=========================
 %% Background
 %%=========================
-background({red,    "Text"}).
-background({black,  "Text"}).
 
+
+background(red,    "Text").
+background(black,  "Text").
 ```
+### Screenshot
+
+[Here](https://raw.githubusercontent.com/lk-geimfari/smokkfiskur/master/other/screen.png) you can find screenshot.
--- src/smokkfiskur.erl
+++ src/smokkfiskur.erl
@@ -5,7 +5,7 @@
 -module(smokkfiskur).
 -author("Lk Geimfari").
 
--export ([print/1, background/1]).
+-export ([print/2, background/2]).
 
 
 -include ("macros.hrl").
@@ -14,27 +14,27 @@
 %% Functions for colored print.
 %%====================================================
 
-print({black,  S})  -> io:format(?BLACK  (S));
-print({red,    S})  -> io:format(?RED    (S));
-print({green,  S})  -> io:format(?GREEN  (S));
-print({orange, S})  -> io:format(?ORANGE (S));
-print({blue,   S})  -> io:format(?BLUE   (S));
-print({purple, S})  -> io:format(?PURPLE (S));
-print({yellow, S})  -> io:format(?YELLOW (S));
-print({cyan,   S})  -> io:format(?CYAN   (S));
-print({grey,   S})  -> io:format(?GREY   (S));
-print({Other,  S})  -> io:format(?_OTHER (S)).
+print(black,  S)  -> io:format(?BLACK  (S));
+print(red,    S)  -> io:format(?RED    (S));
+print(green,  S)  -> io:format(?GREEN  (S));
+print(orange, S)  -> io:format(?ORANGE (S));
+print(blue,   S)  -> io:format(?BLUE   (S));
+print(purple, S)  -> io:format(?PURPLE (S));
+print(yellow, S)  -> io:format(?YELLOW (S));
+print(cyan,   S)  -> io:format(?CYAN   (S));
+print(grey,   S)  -> io:format(?GREY   (S));
+print(Other,  S)  -> io:format(?_OTHER (S)).
 
 
 %%====================================================
 %% Functions for colored background.
 %%====================================================
 
-background({black,  S})  -> io:format(?BLACK_BG  (S));
-background({red,    S})  -> io:format(?RED_BG    (S));
-background({green,  S})  -> io:format(?GREEN_BG  (S));
-background({yellow, S})  -> io:format(?YELLOW_BG (S));
-background({blue,   S})  -> io:format(?BLUE_BG   (S));
-background({purple, S})  -> io:format(?PURPLE_BG (S));
-background({cyan,   S})  -> io:format(?CYAN_BG   (S));
-background({Other,  S})  -> io:format(?_OTHER    (S)).
+background(black,  S)  -> io:format(?BLACK_BG  (S));
+background(red,    S)  -> io:format(?RED_BG    (S));
+background(green,  S)  -> io:format(?GREEN_BG  (S));
+background(yellow, S)  -> io:format(?YELLOW_BG (S));
+background(blue,   S)  -> io:format(?BLUE_BG   (S));
+background(purple, S)  -> io:format(?PURPLE_BG (S));
+background(cyan,   S)  -> io:format(?CYAN_BG   (S));
+background(Other,  S)  -> io:format(?_OTHER    (S)).
