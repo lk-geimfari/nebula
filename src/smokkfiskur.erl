2d7558ba17abbef1321794e6aee3f1bd23f6d99d
Likid Geimfari <likid.geimfari@gmail.com>
2017-01-21 20:23:34 +0300

    Formatting

--- src/smokkfiskur.erl
+++ src/smokkfiskur.erl
@@ -3,40 +3,37 @@
 %%% @copyright (C) 2016, Lk Geimfari (Isaak Uchakaev)
 %%%-------------------------------------------------------------------
 -module(smokkfiskur).
+
 -author("Lk Geimfari").
 
--export ([print/2, background/2]).
+-export([background/2, print/2]).
 
-
--include ("macros.hrl").
+-include("macros.hrl").
 
 %%====================================================
 %% Functions for colored print.
 %%====================================================
 
-print(black,  S)  -> io:format(?BLACK  (S));
-print(red,    S)  -> io:format(?RED    (S));
-print(green,  S)  -> io:format(?GREEN  (S));
-print(orange, S)  -> io:format(?ORANGE (S));
-print(blue,   S)  -> io:format(?BLUE   (S));
-print(purple, S)  -> io:format(?PURPLE (S));
-print(yellow, S)  -> io:format(?YELLOW (S));
-print(cyan,   S)  -> io:format(?CYAN   (S));
-print(grey,   S)  -> io:format(?GREY   (S));
-print(Other,  S)  -> 
-    io:format(?_OTHER(S)), badmatch.
-
+print(black, S) -> io:format(?BLACK(S));
+print(red, S) -> io:format(?RED(S));
+print(green, S) -> io:format(?GREEN(S));
+print(orange, S) -> io:format(?ORANGE(S));
+print(blue, S) -> io:format(?BLUE(S));
+print(purple, S) -> io:format(?PURPLE(S));
+print(yellow, S) -> io:format(?YELLOW(S));
+print(cyan, S) -> io:format(?CYAN(S));
+print(grey, S) -> io:format(?GREY(S));
+print(_, S) -> io:format(?_OTHER(S)), badmatch.
 
 %%====================================================
 %% Functions for colored background.
 %%====================================================
 
-background(black,  S)  -> io:format(?BLACK_BG  (S));
-background(red,    S)  -> io:format(?RED_BG    (S));
-background(green,  S)  -> io:format(?GREEN_BG  (S));
-background(yellow, S)  -> io:format(?YELLOW_BG (S));
-background(blue,   S)  -> io:format(?BLUE_BG   (S));
-background(purple, S)  -> io:format(?PURPLE_BG (S));
-background(cyan,   S)  -> io:format(?CYAN_BG   (S));
-background(Other,  S)  -> 
-    io:format(?_OTHER(S)), badmatch.
+background(black, S) -> io:format(?BLACK_BG(S));
+background(red, S) -> io:format(?RED_BG(S));
+background(green, S) -> io:format(?GREEN_BG(S));
+background(yellow, S) -> io:format(?YELLOW_BG(S));
+background(blue, S) -> io:format(?BLUE_BG(S));
+background(purple, S) -> io:format(?PURPLE_BG(S));
+background(cyan, S) -> io:format(?CYAN_BG(S));
+background(_, S) -> io:format(?_OTHER(S)), badmatch.
